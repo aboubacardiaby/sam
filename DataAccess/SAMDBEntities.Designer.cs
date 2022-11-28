d4c5a8bae4343f30349175857006525334e2b00b
Abou Diaby <adiaby@owens-minor.com>
2022-11-28 14:42:02 -0600

    add version change

--- DataAccess/SAMDBEntities.Designer.cs
+++ DataAccess/SAMDBEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\aboub\source\tfs\SAM\SenegaleseAssociationofMN\DataAccess\SAMDBEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\adiaby\source\repos\sam\sam\DataAccess\SAMDBEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
